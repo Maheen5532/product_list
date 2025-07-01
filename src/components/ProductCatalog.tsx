03283e895d1fdb4353717730b2ec61d37e59217c
Maheen5532 <rapunzel5578@gmail.com>
2025-07-01 22:52:49 +0500

    productlist

--- src/components/ProductCatalog.tsx
+++ src/components/ProductCatalog.tsx
@@ -55,7 +55,7 @@ export const ProductCatalog: React.FC = () => {
       });
 
       const response = await fetch(
-        `http://localhost:3001/api/products?${queryParams}`
+        `https://product-listing-backend-kohl.vercel.app/api/products?${queryParams}`
       );
       if (!response.ok) {
         throw new Error("Failed to fetch products");
